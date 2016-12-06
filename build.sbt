c9b963be8985982b5b2361fb4fc664f42ffa5311
Ryan Williams <ryan.blake.williams@gmail.com>
2016-12-06 17:54:12 +0000

    bump kryo

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ name := "sbt-parent"
 
 organization := "org.hammerlab"
 
-version := "1.2.8"
+version := "1.2.9"
 
 publishTo := {
   val nexus = "https://oss.sonatype.org/"
--- src/main/scala/org/hammerlab/sbt/ParentPlugin.scala
+++ src/main/scala/org/hammerlab/sbt/ParentPlugin.scala
@@ -182,7 +182,7 @@ object ParentPlugin extends AutoPlugin {
             'bdg_utils_cli -> "org.bdgenomics.utils" %% "utils-cli" % "0.2.10",
             'hadoop -> "org.apache.hadoop" % "hadoop-client" % hadoopVersion.value,
             'hadoop_bam -> ("org.seqdoop" % "hadoop-bam" % "7.7.1" exclude("org.apache.hadoop", "hadoop-client")),
-            'kryo -> "com.esotericsoftware.kryo" % "kryo" % "2.21",
+            'kryo -> "com.esotericsoftware.kryo" % "kryo" % "2.24.0",  // Better than Spark's 2.21, which ill-advisedly shades in some minlog classes.
             'mllib -> "org.apache.spark" %% "spark-mllib" % sv,
             'quinine_core -> ("org.bdgenomics.quinine" %% "quinine-core" % "0.0.2" exclude("org.bdgenomics.adam", "adam-core")),
             'scalatest -> "org.scalatest" %% "scalatest" % scalatestVersion.value,
