d00019907804f2a08b42f119ef197a9ad2325493
Ryan Williams <ryan.blake.williams@gmail.com>
2017-08-04 14:55:16 +0000

    add io/math modules, hammerlab short-hand

--- src/main/scala/org/hammerlab/sbt/plugin/Parent.scala
+++ src/main/scala/org/hammerlab/sbt/plugin/Parent.scala
@@ -21,6 +21,9 @@ object Parent
   import Group._
 
   object autoImport {
+
+    def hammerlab(name: String) = "org.hammerlab" ^^ name
+
     val adam = "org.hammerlab.adam" ^^ "core"
     val args4j = "args4j" ^ "args4j"
     val args4s = "org.hammerlab" ^ "args4s"
@@ -31,20 +34,24 @@ object Parent
     val bdg_utils_metrics = "org.bdgenomics.utils" ^^ "utils-metrics-spark2"
     val bdg_utils_misc = "org.bdgenomics.utils" ^^ "utils-misc-spark2"
     val breeze = "org.scalanlp" ^^ "breeze"
+    val bytes = hammerlab("bytes")
     val case_app = "com.github.alexarchambault" ^^ "case-app"
     val cats = "org.typelevel" ^^ "cats"
+    val channel = hammerlab("channel")
     val commons_io = "commons-io" ^ "commons-io"
     val commons_math = "org.apache.commons" ^ "commons-math3"
     val genomic_utils = "org.hammerlab.genomics" ^^ "utils"
     val guava = "com.google.guava" ^ "guava"
     val hadoop_bam = ("org.hammerlab" ^ "hadoop-bam") - hadoop
     val htsjdk = ("com.github.samtools" ^ "htsjdk") - ("org.xerial.snappy" ^ "snappy-java")
-    val iterators = "org.hammerlab" ^^ "iterator"
+    val io = hammerlab("io")
+    val iterators = hammerlab("iterator")
     val loci = ("org.hammerlab.genomics" ^^ "loci") - guava
     val log4j = "org.slf4j" ^ "slf4j-log4j12"
-    val magic_rdds = "org.hammerlab" ^^ "magic-rdds"
+    val magic_rdds = hammerlab("magic-rdds")
+    val math = hammerlab("math")
     val mllib = ("org.apache.spark" ^^ "spark-mllib") - scalatest
-    val paths = "org.hammerlab" ^^ "paths"
+    val paths = hammerlab("paths")
     val parquet_avro = "org.apache.parquet" ^ "parquet-avro"
     val quinine_core = ("org.bdgenomics.quinine" ^^ "quinine-core") - ("org.bdgenomics.adam" ^^ "adam-core")
     val reads = "org.hammerlab.genomics" ^^ "reads"
@@ -53,11 +60,12 @@ object Parent
     val scalautils = "org.scalautils" ^^ "scalautils"
     val shapeless = "com.chuusai" ^^ "shapeless"
     val slf4j = "org.clapper" ^^ "grizzled-slf4j"
-    val spark_bam = "org.hammerlab" ^^ "spark-bam"
-    val spark_commands = "org.hammerlab" ^^ "spark-commands"
-    val spark_util = "org.hammerlab" ^^ "spark-util"
+    val spark_bam = hammerlab("spark-bam")
+    val spark_commands = hammerlab("spark-commands")
+    val spark_util = hammerlab("spark-util")
     val spire = "org.spire-math" ^^ "spire"
-    val string_utils = "org.hammerlab" ^^ "string-utils"
+    val stats = hammerlab("stats")
+    val string_utils = hammerlab("string-utils")
 
     val bdgUtilsVersion = settingKey[String]("org.bdgenomics.utils version to use")
   }
